3fd7d58a2bc53aa6cc1c695bbcddd6f6b06708b8
AsyPredelieva <asy.predelieva@gmail.com>
2020-07-21 23:58:57 +0300

    create home and hero components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.scss'
-import Home from './views/home'
+import Home from './views/home/home'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
---
+++ src/components/hero/hero.styles.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import styled from 'styled-components'
+import heroImg from '../../assets/hero.jpg'
+import shining from '../../assets/shining.png'
+
+export const HeroStyled = styled.div`
+    position: relative;
+    width: 100%;
+    height: 300px;
+    background-image: url(${heroImg});
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    margin-bottom: 40px;
+    box-shadow: inset -1px -1px 7px rgba(0, 0, 0, 0.2);
+
+    &:after {
+        animation: shine ease-in 2s 3 reverse;
+        animation-fill-mode: forwards;
+        content: '';
+        background: url(${shining}) no-repeat;
+        position: absolute;
+        top: -5px;
+        right: 50%;
+        margin-right: -555px;
+        width: 150px;
+        height: 150px;
+        opacity: 0;
+        transform: rotate(30deg);
+    }
+
+    &:active:after {
+        opacity: 0;
+    }
+`
+
+export const Intro = styled.div`
+    width: 45%;
+    text-align: left;
+    font-size: 20px;
+    padding-top: 55px;
+
+    p {
+        margin-bottom: 35px;
+    }
+
+    .cta-btn {
+        font-size: 16px;
+        line-height: 1.4;
+    }
+`
+
+// @keyframes shine {
+//     0% {
+//         opacity: 0;
+//         -webkit-transform: scale(0.5) rotate(0deg);
+//         transform: scale(0.5) rotate(0deg);
+//     }
+//     70% {
+//         opacity: 0.5;
+//         -webkit-transform: scale(1.5) rotate(360deg);
+//         transform: scale(1.5) rotate(360deg);
+//     }
+//     100% {
+//         opacity: 0;
+//         -webkit-transform: scale(0.1) rotate(0deg);
+//         transform: scale(0.1) rotate(0deg);
+//     }
+// }
