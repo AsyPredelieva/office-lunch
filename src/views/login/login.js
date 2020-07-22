c8d77b692de894f7676dface13310d00d2fb7ca8
AsyPredelieva <asy.predelieva@gmail.com>
2020-07-22 09:03:51 +0300

    clean up Login form

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -12,49 +12,14 @@ const Login = () => {
                         <div className='form-row'>
                             <div className='form-field'>
                                 <label htmlFor='username'>Username</label>
-                                <input
-                                    type='text'
-                                    id='username'
-                                    // v-model.trim="username"
-                                    // @blur="$v.username.$touch"
-                                />
+                                <input type='text' id='username' />
                             </div>
-                            <template v-if='$v.username.$error'>
-                                <p
-                                    className='error'
-                                    // v-if="!$v.username.required"
-                                >
-                                    Please enter your name.
-                                </p>
-                                <p
-                                    className='error'
-                                    // v-else-if="!$v.username.minLength"
-                                >
-                                    Min length of your name should be more than 2 symbols
-                                </p>
-                            </template>
                         </div>
                         <div className='form-row'>
                             <div className='form-field'>
                                 <label htmlFor='password'>Password</label>
-                                <input
-                                    type='password'
-                                    id='password'
-                                    // v-model="password"
-                                    // @blur="$v.password.$touch"
-                                />
+                                <input type='password' id='password' />
                             </div>
-                            <template v-if='$v.password.$error'>
-                                <p className='error' v-if='!$v.password.required'>
-                                    Please enter your password.
-                                </p>
-                                <p
-                                    className='error'
-                                    // v-else-if="!$v.password.minLength || !$v.password.maxLength"
-                                >
-                                    Passoword should be between 3 and 16 symbols.
-                                </p>
-                            </template>
                         </div>
                         <button className='cta-btn'>Log in</button>
                     </LoginForm>
