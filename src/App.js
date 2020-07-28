87aa32d025bb89a33b21d02049013793b20efc49
AsyPredelieva <asy.predelieva@gmail.com>
2020-07-28 09:48:21 +0300

    feat/create offerDetails page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.scss'
 import Home from './views/home/home'
-import Offers from './views/offers/offers'
+import Offers from './views/offers/offersList/offersList'
 import Login from './views/login/login'
 import Register from './views/register/register'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import OfferDetails from './views/offers/OfferDetails/OfferDetails'
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                 <Route path='/offers' exact component={Offers} />
                 <Route path='/login' exact component={Login} />
                 <Route path='/register' exact component={Register} />
+                <Route path='/offers/Kamenica' exact component={OfferDetails} />
             </Switch>
         </BrowserRouter>
     )
---
+++ src/views/offers/OfferDetails/OfferDetails.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import PageLayout from '../../page-layout'
+// import CurrentOrder from '../../Orders/CurrentOrder'
+import {
+    OfferDetailsStyled,
+    OfferMenuList,
+    CurrentOrderStyled,
+    OrderDetail,
+    TotalPrice,
+} from './OfferDetails.styles'
+
+const OfferDetails = () => {
+    return (
+        <PageLayout>
+            <OfferDetailsStyled>
+                <div className='container'>
+                    {/* if="!currentOffer"
+                <div>
+                    <Loader />
+                </div> */}
+                    <div>
+                        <form>
+                            <h2>Today's menu in Kamenica</h2>
+                            <OfferMenuList>
+                                <li>
+                                    <h3>Starters</h3>
+                                    <ul>
+                                        <li>
+                                            <span className='name'>Soup</span>
+                                            <OrderDetail>
+                                                <div className='form-field'>
+                                                    <input type='text' placeholder='0' />
+                                                </div>
+                                                <span className='price'>2.50 lv</span>
+                                                <button type='button' className='primary-button'>
+                                                    Add
+                                                </button>
+                                            </OrderDetail>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </OfferMenuList>
+                            {/* if="isAdded" */}
+                            <>
+                                <CurrentOrderStyled>
+                                    <h3>I'll take for lunch:</h3>
+                                    <ul>{/* <CurrentOrder /> */}</ul>
+                                    <TotalPrice>
+                                        <strong>Total sum:</strong>
+                                        <strong>4.50 lv</strong>
+                                    </TotalPrice>
+                                    <button className='cta-btn'>Order</button>
+                                </CurrentOrderStyled>
+                            </>
+                        </form>
+                    </div>
+                </div>
+            </OfferDetailsStyled>
+        </PageLayout>
+    )
+}
+
+export default OfferDetails
