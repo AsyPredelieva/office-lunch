a95e6f003bbd0ebc615783f0b623dca4b1aa2df9
AsyPredelieva <asy.predelieva@gmail.com>
2020-08-02 13:09:59 +0300

    create Input component and use it in Login and Register forms

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,32 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PageLayout from '../PageLayout'
+import Input from '../../components/common/Input/Input'
 import { LoginContainer, InnerContainer, LoginForm } from './Login.styles'
 
-const Login = () => {
-    return (
-        <PageLayout>
-            <LoginContainer>
-                <InnerContainer className='container'>
-                    <h2>Lunch time</h2>
-                    <LoginForm>
-                        <div className='form-row'>
-                            <div className='form-field'>
-                                <label htmlFor='username'>Username</label>
-                                <input type='text' id='username' />
+class Login extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: '',
+            password: '',
+        }
+    }
+
+    onChange = (ev, type) => {
+        const fieldValue = {}
+
+        fieldValue[type] = ev.target.value
+        this.setState(fieldValue)
+    }
+
+    render() {
+        const { username, password } = this.state
+
+        return (
+            <PageLayout>
+                <LoginContainer>
+                    <InnerContainer className='container'>
+                        <h2>Lunch time</h2>
+                        <LoginForm>
+                            <div className='form-row'>
+                                <Input
+                                    type='text'
+                                    label='Username'
+                                    id='username'
+                                    value={username}
+                                    onChange={(e) => this.onChange(e, 'username')}
+                                />
                             </div>
-                        </div>
-                        <div className='form-row'>
-                            <div className='form-field'>
-                                <label htmlFor='password'>Password</label>
-                                <input type='password' id='password' />
+                            <div className='form-row'>
+                                <Input
+                                    type='password'
+                                    label='Password'
+                                    id='password'
+                                    value={password}
+                                    onChange={(e) => this.onChange(e, 'password')}
+                                />
                             </div>
-                        </div>
-                        <button className='cta-btn'>Log in</button>
-                    </LoginForm>
-                </InnerContainer>
-            </LoginContainer>
-        </PageLayout>
-    )
+                            <button className='cta-btn'>Log in</button>
+                        </LoginForm>
+                    </InnerContainer>
+                </LoginContainer>
+            </PageLayout>
+        )
+    }
 }
 
 export default Login
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,62 +1,105 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PageLayout from '../PageLayout'
+import Input from '../../components/common/Input/Input'
 import { RegisterContainer, InnerContainer, GridFull } from './Register.styles'
 
-const Register = () => {
-    return (
-        <PageLayout>
-            <RegisterContainer>
-                <InnerContainer className='container'>
-                    <h2>Register</h2>
-                    <form className='grid-container'>
-                        <div className='grid-half'>
-                            <div className='form-row'>
-                                <div className='form-field'>
-                                    <label htmlFor='Name'>Username</label>
-                                    <input type='text' id='name' />
+class Register extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: '',
+            lastName: '',
+            department: '',
+            email: '',
+            password: '',
+            rePassword: '',
+        }
+    }
+
+    onChange = (ev, type) => {
+        const fieldValue = {}
+
+        fieldValue[type] = ev.target.value
+        this.setState(fieldValue)
+    }
+
+    render() {
+        const { username, lastName, department, email, password, rePassword } = this.state
+
+        return (
+            <PageLayout>
+                <RegisterContainer>
+                    <InnerContainer className='container'>
+                        <h2>Register</h2>
+                        <form className='grid-container'>
+                            <div className='grid-half'>
+                                <div className='form-row'>
+                                    <Input
+                                        type='text'
+                                        label='Username'
+                                        id='username'
+                                        value={username}
+                                        onChange={(e) => this.onChange(e, 'username')}
+                                    />
+                                </div>
+                                <div className='form-row'>
+                                    <Input
+                                        type='text'
+                                        label='Last Name'
+                                        id='lastName'
+                                        value={lastName}
+                                        onChange={(e) => this.onChange(e, 'lastName')}
+                                    />
+                                </div>
+                                <div className='form-row'>
+                                    <Input
+                                        type='text'
+                                        label='Department'
+                                        id='department'
+                                        value={department}
+                                        onChange={(e) => this.onChange(e, 'department')}
+                                    />
                                 </div>
                             </div>
-                            <div className='form-row'>
-                                <div className='form-field'>
-                                    <label htmlFor='last-name'>Last Name</label>
-                                    <input type='text' id='last-name' />
+                            <div className='grid-half'>
+                                <div className='form-row'>
+                                    <Input
+                                        type='email'
+                                        label='Email'
+                                        id='email'
+                                        value={email}
+                                        onChange={(e) => this.onChange(e, 'email')}
+                                    />
+                                </div>
+                                <div className='form-row'>
+                                    <Input
+                                        type='password'
+                                        label='Password'
+                                        id='password'
+                                        value={password}
+                                        onChange={(e) => this.onChange(e, 'password')}
+                                    />
+                                </div>
+                                <div className='form-row'>
+                                    <Input
+                                        type='password'
+                                        label='Repeat Password'
+                                        id='rePassword'
+                                        value={rePassword}
+                                        onChange={(e) => this.onChange(e, 'rePassword')}
+                                    />
                                 </div>
                             </div>
-                            <div className='form-row'>
-                                <div className='form-field'>
-                                    <label htmlFor='department'>Department</label>
-                                    <input type='text' id='department' />
-                                </div>
-                            </div>
-                        </div>
-                        <div className='grid-half'>
-                            <div className='form-row'>
-                                <div className='form-field'>
-                                    <label htmlFor='email'>Email</label>
-                                    <input type='text' id='email' />
-                                </div>
-                            </div>
-                            <div className='form-row'>
-                                <div className='form-field'>
-                                    <label htmlFor='password'>Password</label>
-                                    <input type='password' id='password' />
-                                </div>
-                            </div>
-                            <div className='form-row'>
-                                <div className='form-field'>
-                                    <label htmlFor='re-password'>Repeat password:</label>
-                                    <input type='password' id='re-password' />
-                                </div>
-                            </div>
-                        </div>
-                        <GridFull className='grid-full'>
-                            <button className='cta-btn'>Submit</button>
-                        </GridFull>
-                    </form>
-                </InnerContainer>
-            </RegisterContainer>
-        </PageLayout>
-    )
+                            <GridFull className='grid-full'>
+                                <button className='cta-btn'>Submit</button>
+                            </GridFull>
+                        </form>
+                    </InnerContainer>
+                </RegisterContainer>
+            </PageLayout>
+        )
+    }
 }
 
 export default Register
