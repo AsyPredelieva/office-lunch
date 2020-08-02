2889687dcb6d31569cf5e3ab0062c5168122a117
AsyPredelieva <asy.predelieva@gmail.com>
2020-08-02 19:38:52 +0300

    add Router guards

--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
@@ -7,21 +7,32 @@ import OfferDetails from './pages/Offers/OfferDetails/OfferDetails'
 import Orders from './pages/Orders/Orders'
 import NotFound from './pages/NotFound/NotFound'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import UserContext from './Context'
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path='/' exact component={Home} />
-                <Route path='/login' exact component={Login} />
-                <Route path='/register' exact component={Register} />
-                <Route path='/offers' exact component={OffersList} />
-                <Route path='/offers/Kamenica' exact component={OfferDetails} />
-                <Route path='/orders' exact component={Orders} />
-                <Route path='*' component={NotFound} />
-            </Switch>
-        </BrowserRouter>
-    )
+class Router extends Component {
+    static contextType = UserContext
+
+    render() {
+        const { isAuth } = this.context
+
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/login' exact component={Login} />
+                    <Route path='/register' exact component={Register} />
+                    <Route path='/offers' exact component={OffersList} />
+                    {isAuth && (
+                        <>
+                            <Route path='/offers/Kamenica' exact component={OfferDetails} />
+                            <Route path='/orders' exact component={Orders} />
+                        </>
+                    )}
+                    <Route path='*' component={NotFound} />
+                </Switch>
+            </BrowserRouter>
+        )
+    }
 }
 
-export default App
+export default Router
