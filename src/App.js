884279b3081a708209725b1327284ec96656c409
AsyPredelieva <asy.predelieva@gmail.com>
2020-08-02 23:17:46 +0300

    verify loggedIn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import UserContext from './Context'
+import getCookie from './utils/getCookie'
 
 class App extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            isAuth: false,
+            isAuth: null,
             user: null,
         }
     }
@@ -19,15 +20,46 @@ class App extends Component {
     }
 
     logOut = () => {
-        document.cookie = 'x-auth-token='
+        document.cookie = 'x-auth-token= ; expires=Thu, 01 Jan 1970 00:00:00 GMT'
         this.setState({
             isAuth: false,
             user: null,
         })
     }
 
+    componentDidMount() {
+        const token = getCookie('x-auth-token')
+        console.log(token)
+
+        if (!token) {
+            this.logOut()
+            return
+        }
+
+        fetch('http://localhost:9999/api/user/verify', {
+            method: 'POST',
+            body: JSON.stringify({ token }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((data) => data.json())
+            .then((res) => {
+                res.status
+                    ? this.logIn({
+                          username: res.user.username,
+                          id: res.user._id,
+                      })
+                    : this.logOut()
+            })
+    }
+
     render() {
         const { isAuth, user } = this.state
+
+        if (isAuth === null) {
+            return <div>Loading...</div>
+        }
 
         return (
             <UserContext.Provider value={{ isAuth, user, logIn: this.logIn, logOut: this.logOut }}>
