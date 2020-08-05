7cef1c2eb020c5cdac2d539fc6563fd0224aa226
AsyPredelieva <asy.predelieva@gmail.com>
2020-08-05 23:52:29 +0300

    Header, Nav and Home using hooks

--- src/components/common/Nav/Nav.js
+++ src/components/common/Nav/Nav.js
@@ -1,81 +1,69 @@
-import React, { Component } from 'react'
-import { Link, Redirect } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { NavStyled } from './Nav.styles'
 import UserContext from '../../../Context'
 
-class Nav extends Component {
-    constructor(props) {
-        super(props)
+const Nav = () => {
+    const context = useContext(UserContext)
+    const history = useHistory()
 
-        this.state = {
-            username: '',
-        }
+    const logOut = () => {
+        context.logOut()
+        history.push('/')
     }
 
-    static contextType = UserContext
-
-    logOut = () => {
-        this.context.logOut()
-
-        return <Redirect to='/' />
-    }
-
-    render() {
-        const { user } = this.context
-
-        return (
-            <NavStyled>
-                <input className='menu-btn' type='checkbox' id='menu-btn' />
-                <label className='menu-icon' htmlFor='menu-btn'>
-                    <span className='nav-icon'></span>
-                </label>
-                <ul>
-                    <li>
-                        <Link to='/' exact='true'>
-                            Home
-                        </Link>
-                    </li>
-                    <li className='sub-nav'>
-                        <Link to='/offers'>Offers</Link>
-                        <input className='sub-menu-btn' type='checkbox' id='sub-menu-btn' />
-                        <label className='sub-menu-icon' htmlFor='sub-menu-btn'>
-                            <span className='nav-icon'></span>
-                        </label>
-                        <ul>
-                            <li>
-                                <Link to={'/offers/Kamenica'}>Kamenica</Link>
-                            </li>
-                        </ul>
-                    </li>
-                    {user && user.isAuth ? (
-                        <>
-                            <li className='sub-nav'>
-                                <Link to='/orders'>Orders</Link>
-                            </li>
-                            {/* 
+    return (
+        <NavStyled>
+            <input className='menu-btn' type='checkbox' id='menu-btn' />
+            <label className='menu-icon' htmlFor='menu-btn'>
+                <span className='nav-icon'></span>
+            </label>
+            <ul>
+                <li>
+                    <Link to='/' exact='true'>
+                        Home
+                    </Link>
+                </li>
+                <li className='sub-nav'>
+                    <Link to='/offers'>Offers</Link>
+                    <input className='sub-menu-btn' type='checkbox' id='sub-menu-btn' />
+                    <label className='sub-menu-icon' htmlFor='sub-menu-btn'>
+                        <span className='nav-icon'></span>
+                    </label>
+                    <ul>
+                        <li>
+                            <Link to={'/offers/Kamenica'}>Kamenica</Link>
+                        </li>
+                    </ul>
+                </li>
+                {context.user && context.user.isAuth ? (
+                    <>
+                        <li className='sub-nav'>
+                            <Link to='/orders'>Orders</Link>
+                        </li>
+                        {/* 
                             <li className='sub-nav'>
                                 <Link to='/MyOrders'>My Orders</Link>
                             </li> */}
-                            <li>
-                                <a onClick={this.logOut} className='logout'>
-                                    Logout
-                                </a>
-                            </li>
-                        </>
-                    ) : (
-                        <>
-                            <li>
-                                <Link to='/login'>Login</Link>
-                            </li>
-                            <li>
-                                <Link to='/register'>Register</Link>
-                            </li>
-                        </>
-                    )}
-                </ul>
-            </NavStyled>
-        )
-    }
+                        <li>
+                            <a onClick={logOut} className='logout'>
+                                Logout
+                            </a>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <Link to='/login'>Login</Link>
+                        </li>
+                        <li>
+                            <Link to='/register'>Register</Link>
+                        </li>
+                    </>
+                )}
+            </ul>
+        </NavStyled>
+    )
 }
 
 export default Nav
