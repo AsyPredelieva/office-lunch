92d928c0b09b785931e3082e2b1095a547c49644
AsyPredelieva <asy.predelieva@gmail.com>
2020-08-06 21:52:37 +0300

    get offers data; insert it in offerItem

--- src/pages/Offers/OffersList/OffersList.js
+++ src/pages/Offers/OffersList/OffersList.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import PageLayout from '../../PageLayout'
 import OfferItem from '../../../components/Offers/OfferItem/OfferItem'
 import { OffersContainer, OffersList } from './OffersList.styles'
 
 const Offers = () => {
+    const [offers, setOffers] = useState([])
+    const history = useHistory()
+
+    const getOffers = async () => {
+        const response = await fetch('http://localhost:9999/api/offers')
+
+        if (!response.ok) {
+            history.push('/')
+        } else {
+            const data = await response.json()
+            console.log(data)
+            setOffers(data)
+        }
+    }
+
+    useEffect(() => {
+        getOffers()
+    }, [])
+
+    if (!offers) {
+        return
+    }
+
     return (
         <PageLayout>
             <OffersContainer className='container'>
                 <h2>Get the best</h2>
-                {/* if="!offersList" */}
-                {/* <div>
-                <Loader />
-            </div> */}
+                {!offers && <div>Loader...</div>}
                 <div>
                     <OffersList>
-                        <OfferItem />
+                        {offers.map((offer) => (
+                            <OfferItem key={offer.id} name={offer.name} imgBckgr={offer.imgUrl} />
+                        ))}
                     </OffersList>
                 </div>
             </OffersContainer>
