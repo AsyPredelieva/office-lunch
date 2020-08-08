e5dd6560716b18a74f9eb4eb0b43463c7630edbe
AsyPredelieva <asy.predelieva@gmail.com>
2020-08-08 23:32:34 +0300

    change CurrentOrder component

--- src/components/Orders/CurrentOrder/CurrentOrder.js
+++ src/components/Orders/CurrentOrder/CurrentOrder.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { OrderDetail } from './CurrentOrder.styles'
 
-const CurrentOrder = () => {
+const CurrentOrder = ({ name, count, price, removeCurrItem }) => {
     return (
         <li>
-            {/* <span slot='my-current-order'>CurrentOrder name</span> */}
+            <span slot='my-current-order'>{name}</span>
             <OrderDetail>
-                <span>2</span>
-                <span className='price'>5.50 lv</span>
-                <span className='remove-icon'>
+                <span>{count}</span>
+                <span className='large price'>{price} lv</span>
+                <a onClick={removeCurrItem} className='remove-icon'>
+                    X
                     {/* <svg
                         xmlns='http://www.w3.org/2000/svg'
                         xmlns:xlink='http://www.w3.org/1999/xlink'
@@ -17,7 +18,7 @@ const CurrentOrder = () => {
                         fill='#74b4c4'>
                         <polygon points='.75 .043 .043 .75 .398 1.102 5.293 6 .043 11.25 .75 11.957 6 6.707 10.895 11.605 11.25 11.957 11.957 11.25 11.605 10.895 6.707 6 11.957 .75 11.25 .043 6 5.293 1.102 .398' />
                     </svg> */}
-                </span>
+                </a>
             </OrderDetail>
         </li>
     )
