d3465d00d65629bb4d00e8fba4fc2bafd73e505f
AsyPredelieva <asy.predelieva@gmail.com>
2020-08-15 12:12:54 +0300

    fix responsive issues

--- src/components/Home/Quotes/QuoteItem/QuoteItem.styles.js
+++ src/components/Home/Quotes/QuoteItem/QuoteItem.styles.js
@@ -6,19 +6,11 @@ export const QuoteStyled = styled.p`
     margin-bottom: 0;
     position: relative;
 
-    @media screen and(max-width: 768px) {
-        font-size: 18px;
-    }
-
     small {
         font-size: 18px;
         font-style: normal;
         position: absolute;
         right: 0px;
         bottom: 20px;
-
-        @media screen and(max-width: 768px) {
-            font-size: 14px;
-        }
     }
 `
